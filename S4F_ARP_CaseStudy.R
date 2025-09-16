7dbe258061bb99a0c0831ed46de0aa76907393d4
TaylorAkers <taylor.akers@usda.gov>
2025-09-16 16:09:53 -0600

    outlined 1-4

--- S4F_ARP_CaseStudy.R
+++ S4F_ARP_CaseStudy.R
@@ -1,1 +1,250 @@
-## Page 1
+# description ----
+
+# this script was created by Taylor Akers (ORISE fellow with USFS, RMRS), September 2025
+
+# this script is to outline the data analysis workflow for the 
+# Seeds 4 the Future ARP Case Study for teaching purposes
+
+# see the associated S4F_ARP_CS_overview.doc for more details 
+# input data can be found in S4F_ARP_CS_data.zip folder 
+
+
+# (1) setup ----
+
+# first go to your output pane (bottom right) -> packages -> install
+# type the name of each to search, then install, then run the library for each (below)
+# libraries will need to be run with each new session, but package downloaded only once 
+
+library(terra) # primary spatial data management package
+library(tidyterra) # dplyr-style data management for spatial data
+library(ggplot2) # for fancy plotting
+
+
+# (2) create AOI ----
+
+# the Area of Interest (AOI) for this case study is the Arapaho-Roosevelt National Forest (ARP)
+# we will first read in a larger shapefile of all the NFs in Colorado
+  # you will convert the shapefile (.shp) into a "vector" object 
+  # vector objects can be points, lines, or polygons
+  # vector objects have "geometries" (the unique shapes) and "attributes" (metadata)
+# then learn how to filter the data for just the ARP polygon and it's associated attributes 
+
+## ARP ----
+### read file ----
+NF_CO_vect <- vect("NF_CO_vect.shp")
+
+### viz ----
+plot(NF_CO_vect)
+
+### see unique names ----
+unique(NF_CO_vect$COMMONNAME)
+
+### select for just ARP ----
+ARP_vect <- NF_CO_vect %>%
+  filter(COMMONNAME == "Arapaho and Roosevelt National Forests")
+
+plot(ARP_vect)
+
+## reference points ----
+# we will add these 2 points as references, they represent Fort Collins and Boulder
+# you could do all sorts of fancy additions to a plot like this
+# for ex: color code the ranger districts, add scale bar/north arrow/labels/etc
+
+### read file ----
+CO_refs_vect <- vect("CO_refs_vect.shp")
+
+### viz ----
+plot(ARP_vect)
+points(CO_refs_vect, pch = 19, col = "purple", cex = 1.5)
+
+
+# (3) pre-process data ----
+
+# here we will pre-process the data layers that will be combined in step 4
+# most of the data that you will read in has already been pre-processed for this tutorial
+  # and only requires a little more processing for demonstration 
+
+# each data layer is a .tif file, which we will convert into a "raster" object
+# a raster object is a grid of pixels, with a 30x30 meter resolution (in this ex)
+  # which means each pixel represents 900 square meters of the earth's surface
+  # at that specific location 
+
+
+## (3.a) risk ----
+# using flame length exceedance probability of 8 ft (FLEP8)
+# as our proxy for risk of local extinction due to high severity wildfire
+# cell values = probability of flame length > 8 ft
+
+### read file ----
+ARP_risk_score_rast <- rast("ARP_risk_score_rast.tif")
+
+### viz ----
+plot(ARP_risk_score_rast)
+polys(ARP_vect, col = "black", alpha=0.01, lwd=2)
+points(CO_refs_vect, pch = 19, col = "purple", cex = 1.5)
+
+
+## (3.b) slope ----
+# this slope raster was generated using a 
+# digital elevation model (DEM), downloaded from The National Map
+# cell values = degrees
+
+### read ----
+ARP_slope_score_rast <- rast("ARP_slope_score_rast.tif")
+
+### viz ----
+plot(ARP_slope_score_rast)
+polys(ARP_vect, col = "black", alpha=0.01, lwd=2)
+points(CO_refs_vect, pch = 19, col = "purple", cex = 1.5)
+
+
+## (3.c) road ----
+# this road raster was generated from a roads shapefile (lines)
+# if time allows, go through the following steps
+# if not, skip to "write & read" below
+
+# read
+CO_road_rast <- rast("CO_road_rast.tif") 
+plot(CO_road_rast)
+
+### crop & mask ----
+ARP_road_rast <- crop(CO_road_rast, ARP_vect, mask=TRUE)
+plot(ARP_road_rast)
+
+### distance ----
+# we will calculate the distance to nearest road for each raster cell (pixel)
+ARP_road_dist_rast <- distance(ARP_road_rast, unit="m", method="haversine") 
+plot(ARP_road_dist_rast)
+# cell values = distance to nearest road (in meters)
+
+### classify ----
+# now going to set a 1 mi threshold, and make all other values NA
+# 1 mile = 1609.34 meters (second to last value of col 2 & threshold value for score)
+1609.34/11 # = 146.3036 (start of col 2 & breaks for cols 1 and 2)
+1609.34-146.3036 # = 1463.036 (end of col 1)
+minmax(ARP_road_dist_rast) # min = 0, ARP max = 37416.17 (end of col 2)
+
+# set sequence
+# from, to, becomes
+road_sequence = c(c(seq(0, 1463.036, 146.3036),1609.34), c(seq(146.3036, 1609.34, 146.3036),37416.17), c(seq(0, 1, 0.1),NA))
+# 1609.34 is the cutoff (final row of col 1, second to last of col 2)
+# 37416.17 is the max road length in the ARP (end of col 2)
+# from 1609.34 to 37416.17 becomes NA, while the other lower values become 0-1
+
+# create matrix
+road_matrix = matrix(road_sequence, ncol=3, byrow=FALSE) 
+
+# classify
+ARP_road_class_rast = classify(ARP_road_dist_rast, road_matrix, right=NA, others=NA)
+plot(ARP_road_class_rast)
+
+### inverse score ---- 
+ARP_road_inv_rast <- (1 - ARP_road_class_rast)
+plot(ARP_road_inv_rast)
+plot(is.na(ARP_road_inv))
+
+### crop & mask ----
+# do again because distance made a buffer that goes beyond ARP
+ARP_road_score_rast <- crop(ARP_road_inv_rast, ARP_vect, mask=TRUE)
+
+### viz ----
+plot(ARP_road_score_rast)
+polys(ARP_vect, col = "black", alpha=0.01, lwd=2)
+points(CO_refs_vect, pch = 19, col = "purple", cex = 1.5)
+
+### write & read ----
+# writeRaster(ARP_road_score_rast, "ARP_road_score_rast.tif")
+# ARP_road_score_rast <- rast("ARP_road_score_rast.tif")
+
+
+## (3.d) tree height ----
+# using existing vegetation height (EVH) from LANDFIRE
+# prior processing:
+  # filter for only trees 
+  # convert heigh units from m to ft
+  # filter only trees >= 20 ft
+# cell values = height in meters
+
+### read ----
+ARP_height_rast <- rast("ARP_height_rast.tif")
+
+### viz ----
+# make special color pallet 
+green_palette <- colorRampPalette(c("lightgreen", "darkgreen"))(25)  # Creates 25 shades of green
+
+# see height range
+plot(ARP_height_rast, col = green_palette)
+
+### classify ----
+# we want to treat any tree >= 20 ft equally, so make all...
+  # cell value = 100
+
+# see current height range
+minmax(ARP_height_rast)
+  # min = 22.96588
+  # max = 82.02100
+
+# set sequence
+# from, to, becomes
+height_sequence = c(22.96588,82.02100,100)
+
+# create matrix 
+height_matrix = matrix(height_sequence, ncol=3, byrow=TRUE)
+
+# classify
+ARP_height_score_rast = classify(ARP_height_rast, height_matrix, right=NA, others=NA)
+
+# see values
+unique(ARP_height_score_rast) # 100
+
+### viz ----
+# see classified values
+plot(ARP_height_score_rast, col = "darkgreen")
+polys(ARP_vect, col = "black", alpha=0.01, lwd=2)
+points(CO_refs_vect, pch = 19, col = "purple", cex = 1.5)
+
+### write & read ----
+# writeRaster(ARP_height_score_rast, "ARP_height_score_rast.tif")
+# ARP_height_score_rast <- rast("ARP_height_score_rast")
+
+
+# (4) combine data ----
+# now we will do some raster math
+  # sum the cell values among the 4 data layers
+  # if a cell for any layer = NA, then NA is returned
+
+combined_rast <- sum(ARP_risk_score_rast, ARP_slope_score_rast, ARP_road_score_rast, ARP_height_score_rast, na.rm=FALSE)
+
+### viz ----
+plot(combined_rast)
+
+# note, all these areas meet our basic criteria (3.a - 3.d)
+# however, it is still too large of an area to scout
+# so, we must narrow it down further
+
+### explore ----
+# see frequency of values
+freq(combined_rast)
+  # value   count
+  # 100   73573    - these cells score 0 for risk, slope, and road (min requirements met)
+  # 101 1456051
+  # 102 2032222
+  # 103  199972    - these cells score high for risk, slope, and road (highest priority)
+
+# how large of an area is this?
+73573+1456051+2032222+199972 # = 3761818 cells with values (min requirements met)
+3761818*900 # = 3385636200 square meters
+3385636200/4046.86 # 4046.86 m/acre = 836,608.2 acres
+
+# (5) filter areas for PCUs ----
+
+
+
+
+
+
+
+
+
+
+
