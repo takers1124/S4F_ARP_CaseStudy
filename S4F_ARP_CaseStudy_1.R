35b539b621ab0ce826bafdda1b3e25b22d3079b5
TaylorAkers <taylor.akers@usda.gov>
2025-09-23 10:52:22 -0600

    Created 2 new files for the full script

---
+++ S4F_ARP_CaseStudy_1.R
@@ -0,0 +1,17 @@
+# desctiption ----
+
+# Seeds 4 the Future ARP Case Study, the full script
+# part 1, create Potential Collection Units (PCUs)
+
+# this script was created by Taylor Akers (ORISE fellow with USFS, RMRS), Fall-Winter 2025
+
+# see ARP_CaseStudy_Overview_RMRS.pdf for a full description of the project
+
+# (1) setup ----
+
+# to be filled in later... 
+
+
+
+
+
---
+++ S4F_ARP_CaseStudy_2.R
@@ -0,0 +1,12 @@
+# desctiption ----
+
+# Seeds 4 the Future ARP Case Study, the full script
+# part 2, climate match Potential Planting Units (PPUs)
+
+# this script was created by Taylor Akers (ORISE fellow with USFS, RMRS), Fall-Winter 2025
+
+# see ARP_CaseStudy_Overview_RMRS.pdf for a full description of the project
+
+# (1) setup ----
+
+# to be filled in later..
